eb8e45241a39a5dff284dd8336b48d54a1f648ba
Z-tech2 <107017335+Z-tech2@users.noreply.github.com>
2022-07-14 14:42:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
 def selfDestruct():
-#this PERMANENTLY deletes the running file
-path = os.path.abspath(__file__)
+ #this PERMANENTLY deletes the running file
+ path = os.path.abspath(__file__)
  os.system("del " + path)
